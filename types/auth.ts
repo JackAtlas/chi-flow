331fb8efc49154a0f82426afa94da02c047620ec
JackAtlas <jackatlas8864@gmail.com>
2025-03-13 17:26:23 +0800

    some enum

---
+++ types/auth.ts
@@ -0,0 +1,9 @@
+export enum AuthStatus {
+  AUTHENTICATED = 'AUTHENTICATED',
+  UNAUTHENTICATED = 'UNAUTHENTICATED',
+  LOADING = 'LOADING'
+}
+
+export enum AuthErrorText {
+  UNAUTHENTICATED = 'unathenticated'
+}
---
+++ types/workflow.ts
@@ -0,0 +1,10 @@
+export enum WorkflowStatus {
+  DRAFT = 'DRAFT',
+  PUBLISHED = 'PUBLISHED'
+}
+
+export enum WorkflowCreateResultText {
+  CREATING = 'Creating workflow...',
+  CREATE_SUCCESS = 'Workflow created',
+  CREATE_FAIL = 'Failed to create workflow'
+}
