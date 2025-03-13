e1955d5db35b138d6c1ef8799da96612f01ff8aa
JackAtlas <jackatlas8864@gmail.com>
2025-03-13 17:34:24 +0800

    fix active sidebaritem error

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -44,11 +44,11 @@ const routes = [
 ]
 function DesktopSidebar() {
   const pathname = usePathname()
-  const activeRoute = routes.find(
-    (route) =>
-      (route.href.length > 0 && pathname.includes(route.href)) ||
-      routes[0]
-  )
+  const activeRoute =
+    routes.find(
+      (route) =>
+        route.href.length > 0 && pathname.includes(route.href)
+    ) || routes[0]
   return (
     <div className="hidden relative md:block min-w-[280px] max-w-[280px] h-screen overflow-hidden w-full bg-primary/5 dark:bg-secondary/30 dark:text-foreground text-muted-foreground border-r-2 border-separate">
       <div className="flex items-center justify-center gap-2 border-b-[1px] border-separate p-4">
