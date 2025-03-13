1356e8e31418bcb2d624e0eff300193b735392dc
JackAtlas <jackatlas8864@gmail.com>
2025-03-13 19:22:54 +0800

    chinese

--- types/workflow.ts
+++ types/workflow.ts
@@ -4,7 +4,13 @@ export enum WorkflowStatus {
 }
 
 export enum WorkflowCreateResultText {
-  CREATING = 'Creating workflow...',
-  CREATE_SUCCESS = 'Workflow created',
-  CREATE_FAIL = 'Failed to create workflow'
+  CREATING = '正在创建工作流……',
+  CREATE_SUCCESS = '工作流已创建',
+  CREATE_FAIL = '工作流创建失败'
+}
+
+export enum WorkflowDeleteResultText {
+  DELETING = '正在删除工作流……',
+  DELETE_SUCCESS = '工作流已删除',
+  DELETE_FAIL = '工作流删除失败'
 }
