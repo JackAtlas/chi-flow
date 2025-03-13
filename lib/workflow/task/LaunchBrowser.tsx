c24b7093bf58037e87541df6497cf456d03e3d05
JackAtlas <jackatlas8864@gmail.com>
2025-03-13 21:48:52 +0800

    launch browser type node registry

---
+++ lib/workflow/task/LaunchBrowser.tsx
@@ -0,0 +1,20 @@
+import { TaskParamType, TaskType } from '@/types/task'
+import { GlobeIcon, LucideProps } from 'lucide-react'
+
+export const LaunchBrowserTask = {
+  type: TaskType.LAUNCH_BROWSER,
+  label: '启动浏览器',
+  icon: (props: LucideProps) => (
+    <GlobeIcon className="stroke-pink-400" {...props} />
+  ),
+  isEntryPoint: true,
+  inputs: [
+    {
+      name: '网址',
+      type: TaskParamType.STRING,
+      helperText: 'eg: https://www.taobao.com',
+      required: true,
+      hideHandle: true
+    }
+  ]
+}
