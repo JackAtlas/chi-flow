6df6043cb0198237b7e2c7f3c38eac44ef2d25d2
JackAtlas <jackatlas8864@gmail.com>
2025-03-14 16:56:27 +0800

    browser instance type param

--- app/workflow/_components/nodes/NodeParamField.tsx
+++ app/workflow/_components/nodes/NodeParamField.tsx
@@ -5,6 +5,7 @@ import React, { useCallback } from 'react'
 import StringParam from './param/StringParam'
 import { useReactFlow } from '@xyflow/react'
 import { AppNode } from '@/types/appNode'
+import BrowserInstanceParam from './param/BrowserInstanceParam'
 
 function NodeParamField({
   param,
@@ -34,6 +35,14 @@ function NodeParamField({
         <StringParam
           param={param}
           value={value}
+          updateNodeParamValue={updateNodeParamValue}
+        />
+      )
+    case TaskParamType.BROWSER_INSTANCE:
+      return (
+        <BrowserInstanceParam
+          param={param}
+          value={''}
           updateNodeParamValue={updateNodeParamValue}
         />
       )
