98bf1e84b9aded8838e2b2c9970a99e404e5eb18
JackAtlas <jackatlas8864@gmail.com>
2025-03-15 16:49:58 +0800

    validation error handle

--- components/hooks/useExecutionPlan.tsx
+++ components/hooks/useExecutionPlan.tsx
@@ -1,19 +1,51 @@
-import { FlowToExecutionPlan } from '@/lib/workflow/executionPlan'
+import {
+  FlowToExecutionPlan,
+  FlowToExecutionPlanValidationError
+} from '@/lib/workflow/executionPlan'
 import { AppNode } from '@/types/appNode'
 import { useReactFlow } from '@xyflow/react'
 import { useCallback } from 'react'
+import useFlowValidation from './useFlowValidation'
+import { toast } from 'sonner'
 
 const useExecutionPlan = () => {
   const { toObject } = useReactFlow()
 
+  const { setInvalidInputs, clearErrors } = useFlowValidation()
+
+  const handleError = useCallback(
+    (error: any) => {
+      switch (error.type) {
+        case FlowToExecutionPlanValidationError.NO_ENTRY_POINT:
+          toast.error('缺少起始点')
+          break
+        case FlowToExecutionPlanValidationError.INVALID_INPUTS:
+          toast.error('存在缺值的输入框')
+          setInvalidInputs(error.invalidElements)
+          break
+        default:
+          toast.error('未知错误')
+      }
+    },
+    [setInvalidInputs]
+  )
+
   const generateExecutionPlan = useCallback(() => {
     const { nodes, edges } = toObject()
-    const { executionPlan } = FlowToExecutionPlan(
+    const { executionPlan, error } = FlowToExecutionPlan(
       nodes as AppNode[],
       edges
     )
+
+    if (error) {
+      handleError(error)
+      return null
+    }
+
+    clearErrors()
+
     return executionPlan
-  }, [toObject])
+  }, [toObject, handleError, clearErrors])
 
   return generateExecutionPlan
 }
--- lib/workflow/executionPlan.ts
+++ lib/workflow/executionPlan.ts
@@ -1,4 +1,4 @@
-import { AppNode } from '@/types/appNode'
+import { AppNode, AppNodeMissingInputs } from '@/types/appNode'
 import {
   WorkflowExecutionPlan,
   WorkflowExecutionPlanPhase
@@ -6,8 +6,16 @@ import {
 import { Edge, getIncomers } from '@xyflow/react'
 import { TaskRegistry } from './task/registry'
 
+export enum FlowToExecutionPlanValidationError {
+  'NO_ENTRY_POINT',
+  'INVALID_INPUTS'
+}
 type FlowToExecutionPlanType = {
   executionPlan?: WorkflowExecutionPlan
+  error?: {
+    type: FlowToExecutionPlanValidationError
+    invalidElements?: AppNodeMissingInputs[]
+  }
 }
 
 export function FlowToExecutionPlan(
@@ -19,10 +27,24 @@ export function FlowToExecutionPlan(
   )
 
   if (!entryPoint) {
-    throw new Error('TODO: HANDLE THIS ERROR')
+    return {
+      error: {
+        type: FlowToExecutionPlanValidationError.NO_ENTRY_POINT
+      }
+    }
   }
 
+  const inputsWithErrors: AppNodeMissingInputs[] = []
   const planned = new Set<string>()
+
+  const invalidInputs = getInvalidInputs(entryPoint, edges, planned)
+  if (invalidInputs.length > 0) {
+    inputsWithErrors.push({
+      nodeId: entryPoint.id,
+      inputs: invalidInputs
+    })
+  }
+
   const executionPlan: WorkflowExecutionPlan = [
     {
       phase: 1,
@@ -59,7 +81,10 @@ export function FlowToExecutionPlan(
             currentNode.id,
             invalidInputs
           )
-          throw new Error('TODO: HANDLE THIS ERROR 1')
+          inputsWithErrors.push({
+            nodeId: currentNode.id,
+            inputs: invalidInputs
+          })
         } else {
           continue
         }
@@ -71,6 +96,15 @@ export function FlowToExecutionPlan(
       planned.add(node.id)
     }
     executionPlan.push(nextPhase)
+  }
+
+  if (inputsWithErrors.length > 0) {
+    return {
+      error: {
+        type: FlowToExecutionPlanValidationError.INVALID_INPUTS,
+        invalidElements: inputsWithErrors
+      }
+    }
   }
 
   return { executionPlan }
