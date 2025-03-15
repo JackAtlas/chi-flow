42c0fcf422d0c2bddd8b01f4f6c02a1add61e1f4
JackAtlas <jackatlas8864@gmail.com>
2025-03-15 14:25:10 +0800

    set task type

--- lib/workflow/task/registry.tsx
+++ lib/workflow/task/registry.tsx
@@ -1,8 +1,14 @@
+import { TaskType } from '@/types/task'
 import { ExtractTypeFromElement } from './ExtractTextFromElement'
 import { LaunchBrowserTask } from './LaunchBrowser'
 import { PageToHtmlTask } from './PageToHTML'
+import { WorkflowTask } from '@/types/workflow'
 
-export const TaskRegistry = {
+type Registry = {
+  [K in TaskType]: WorkflowTask
+}
+
+export const TaskRegistry: Registry = {
   LAUNCH_BROWSER: LaunchBrowserTask,
   PAGE_TO_HTML: PageToHtmlTask,
   EXTRACT_TEXT_FROM_ELEMENT: ExtractTypeFromElement
