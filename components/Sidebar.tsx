87c3a4d5b6bf3a43ef64d2d5336745471467925b
JackAtlas <jackatlas8864@gmail.com>
2025-03-22 08:53:17 +0800

    put some gap in sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -59,7 +59,7 @@ function DesktopSidebar() {
       <div className="p-2">
         <UserAvailableCreditsBadge />
       </div>
-      <div className="flex flex-col p-2">
+      <div className="flex flex-col p-2 gap-2">
         {routes.map((route) => (
           <Link
             key={route.href}
