62bf4ed23e894e3bf38ec091614afe4063b7b757
JackAtlas <jackatlas8864@gmail.com>
2026-04-28 11:21:50 +0800

    feat: meta data

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,12 @@
 import "./styles.css"
 import { AppProviders } from "@/components/providers/app-providers"
+import { Metadata } from "next"
 import { Toaster } from "sonner"
+
+export const metadata: Metadata = {
+  title: "ChiFlow",
+  description: "一款工作流管理工具",
+}
 
 export default function RootLayout({
   children,
