8e33486c81a23d975af17bdbb5b9127eff62328f
JackAtlas <jackatlas8864@gmail.com>
2026-04-28 11:24:44 +0800

    feat: setup tanstack react query

--- apps/web/components/providers/app-providers.tsx
+++ apps/web/components/providers/app-providers.tsx
@@ -1,8 +1,14 @@
 "use client"
 
-import { ReactNode } from "react"
+import { ReactNode, useState } from "react"
 import { ThemeProvider } from "../theme-provider"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 export function AppProviders({ children }: { children: ReactNode }) {
-  return <ThemeProvider>{children}</ThemeProvider>
+  const [queryClient] = useState(() => new QueryClient())
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>{children}</ThemeProvider>
+    </QueryClientProvider>
+  )
 }
