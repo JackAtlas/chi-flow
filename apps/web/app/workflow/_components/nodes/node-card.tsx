24511028d1afaff17ed7a11cccc737fb32d68c78
JackAtlas <jackatlas8864@gmail.com>
2026-04-30 00:27:57 +0800

    feat: creating react-flow nodes

---
+++ apps/web/app/workflow/_components/nodes/node-card.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { cn } from '@workspace/ui/lib/utils'
+import { useReactFlow } from '@xyflow/react'
+import { ReactNode } from 'react'
+
+export default function NodeCard({
+  children,
+  isSelected,
+  nodeId
+}: {
+  children: ReactNode
+  isSelected: boolean
+  nodeId: string
+}) {
+  const { getNode, setCenter } = useReactFlow()
+  return (
+    <div
+      className={cn(
+        'flex w-105 border-separate cursor-pointer flex-col gap-1 rounded-md border-2 bg-background text-xs',
+        isSelected && 'border-primary'
+      )}
+      onDoubleClick={() => {
+        const node = getNode(nodeId)
+        if (!node) return
+        const { position, measured } = node
+        if (!position || !measured) return
+        const { width, height } = measured
+        const x = position.x + width! / 2
+        const y = position.y + height! / 2
+        if (x === undefined || y === undefined) return
+
+        setCenter(x, y, {
+          zoom: 1,
+          duration: 500
+        })
+      }}
+    >
+      {children}
+    </div>
+  )
+}
