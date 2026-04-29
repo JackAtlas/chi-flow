97a36cc55f1ed261d3be509cddd972b9ec1e2c8b
JackAtlas <jackatlas8864@gmail.com>
2026-04-30 00:24:57 +0800

    refactor: improve auth proxy

--- apps/web/proxy.ts
+++ apps/web/proxy.ts
@@ -1,19 +1,34 @@
-import { NextRequest, NextResponse } from "next/server"
-import { auth } from "./lib/auth/auth"
-import { headers } from "next/headers"
+import { NextRequest, NextResponse } from 'next/server'
+import { auth } from './lib/auth/auth'
+import { headers } from 'next/headers'
 
 export async function proxy(request: NextRequest) {
   const session = await auth.api.getSession({
-    headers: await headers(),
+    headers: await headers()
   })
 
-  if (!session) {
-    return NextResponse.redirect(new URL("/signIn", request.url))
+  const isLoggedIn = !!session?.user
+  const { pathname } = request.nextUrl
+
+  const authPages = ['/signUp', '/signIn', '/adminSignIn']
+
+  const isAuthPage = authPages.includes(pathname)
+
+  if (isAuthPage) {
+    if (isLoggedIn) {
+      return NextResponse.redirect(new URL('/', request.url))
+    }
+
+    return NextResponse.next()
+  }
+
+  if (!isLoggedIn) {
+    return NextResponse.redirect(new URL('/signIn', request.url))
   }
 
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: ["/"],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
 }
