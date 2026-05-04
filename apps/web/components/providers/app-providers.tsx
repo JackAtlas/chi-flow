fd418c3354b36e6119828ef62d3bfd0b4c8cddca
JackAtlas <jackatlas8864@gmail.com>
2026-05-04 15:38:53 +0800

    feat: top loader

--- apps/web/components/providers/app-providers.tsx
+++ apps/web/components/providers/app-providers.tsx
@@ -3,11 +3,13 @@
 import { ReactNode, useState } from 'react'
 import { ThemeProvider } from '../theme-provider'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import NextTopLoader from 'nextjs-toploader'
 
 export function AppProviders({ children }: { children: ReactNode }) {
   const [queryClient] = useState(() => new QueryClient())
   return (
     <QueryClientProvider client={queryClient}>
+      <NextTopLoader color="#10b981" showSpinner={false}></NextTopLoader>
       <ThemeProvider>{children}</ThemeProvider>
     </QueryClientProvider>
   )
