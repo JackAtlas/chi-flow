c8c39942ac193f9305ecad0ede1cf24f7d6d0415
JackAtlas <jackatlas8864@gmail.com>
2026-05-08 22:14:21 +0800

    chore: next.js output standalone

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["@workspace/ui"],
+  transpilePackages: ['@workspace/ui'],
+  output: 'standalone',
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "api.dicebear.com",
-        pathname: "/9.x/avataaars-neutral/webp",
-      },
-    ],
-  },
+        protocol: 'https',
+        hostname: 'api.dicebear.com',
+        pathname: '/9.x/avataaars-neutral/webp'
+      }
+    ]
+  }
 }
 
 export default nextConfig
