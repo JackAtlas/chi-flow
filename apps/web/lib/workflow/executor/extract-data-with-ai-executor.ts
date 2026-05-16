e490ffd6963172a5c7aa78005cc812d5a7bd1f51
JackAtlas <jackatlas8864@gmail.com>
2026-05-16 16:34:00 +0800

    feat: connect OpenRouter AI API

--- apps/web/lib/workflow/executor/extract-data-with-ai-executor.ts
+++ apps/web/lib/workflow/executor/extract-data-with-ai-executor.ts
@@ -2,7 +2,7 @@ import type { ExecutionEnvironment } from '@/types/executor'
 import type { ExtractDataWithAITask } from '../task/extract-data-with-ai'
 import prisma from '@/lib/prisma'
 import { symmetricDecrypt } from '@/lib/encryption'
-// import OpenAI from 'openai'
+import OpenAI from 'openai'
 
 export async function ExtractDataWithAIExecutor(
   environment: ExecutionEnvironment<typeof ExtractDataWithAITask>
@@ -38,50 +38,50 @@ export async function ExtractDataWithAIExecutor(
       return false
     }
 
-    const mockExtractedData = {
-      usernameSelector: '#username',
-      passwordSelector: '#password',
-      loginSelector: 'body > div > form > input.btn.btn-primary'
-    }
-    environment.setOutput('Extracted data', JSON.stringify(mockExtractedData))
-
-    // const openai = new OpenAI({
-    //   baseURL: 'https://api.deepseek.com',
-    //   apiKey: plainCredentialValue
-    // })
-
-    // const response = await openai.chat.completions.create({
-    //   model: 'deepseek-v4-flash',
-    //   messages: [
-    //     {
-    //       role: 'system',
-    //       content:
-    //         'You are a webscraper helper that extracts data from HTML or text. You will be given a piece of text or HTML content as input and also the prompt with the data you have to extract. The response should always be only the extracted data as a JSON array or object, without any additional words or explanations. Analyze the input carefully and extract data precisely based on the prompt. If no data is found, return an empty JSON array. Work only with the provided content and ensure the output is always a valid JSON array without any surrounding text'
-    //     },
-    //     {
-    //       role: 'user',
-    //       content
-    //     },
-    //     {
-    //       role: 'user',
-    //       content: prompt
-    //     }
-    //   ],
-    //   temperature: 1
-    // })
-
-    // environment.log.info(`Prompt tokens: ${response.usage?.prompt_tokens}`)
-    // environment.log.info(
-    //   `Completition tokens: ${response.usage?.completion_tokens}`
-    // )
-
-    // const result = response.choices[0]?.message.content
-    // if (!result) {
-    //   environment.log.error('Empty response from AI')
-    //   return false
+    // const mockExtractedData = {
+    //   usernameSelector: '#username',
+    //   passwordSelector: '#password',
+    //   loginSelector: 'body > div > form > input.btn.btn-primary'
     // }
+    // environment.setOutput('Extracted data', JSON.stringify(mockExtractedData))
 
-    // environment.setOutput('Extracted data', result)
+    const openai = new OpenAI({
+      baseURL: 'https://openrouter.ai/api/v1',
+      apiKey: plainCredentialValue
+    })
+
+    const response = await openai.chat.completions.create({
+      model: 'openrouter/free',
+      messages: [
+        {
+          role: 'system',
+          content:
+            'You are a webscraper helper that extracts data from HTML or text. You will be given a piece of text or HTML content as input and also the prompt with the data you have to extract. The response should always be only the extracted data as a JSON array or object, without any additional words or explanations. Analyze the input carefully and extract data precisely based on the prompt. If no data is found, return an empty JSON array. Work only with the provided content and ensure the output is always a valid JSON array without any surrounding text'
+        },
+        {
+          role: 'user',
+          content
+        },
+        {
+          role: 'user',
+          content: prompt
+        }
+      ],
+      temperature: 1
+    })
+
+    environment.log.info(`Prompt tokens: ${response.usage?.prompt_tokens}`)
+    environment.log.info(
+      `Completition tokens: ${response.usage?.completion_tokens}`
+    )
+
+    const result = response.choices[0]?.message.content
+    if (!result) {
+      environment.log.error('Empty response from AI')
+      return false
+    }
+
+    environment.setOutput('Extracted data', result)
 
     console.log('ExtractDataWithAITask End')
     return true
